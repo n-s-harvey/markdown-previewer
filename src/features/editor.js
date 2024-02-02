bdef48621a2b91ca33f2d9d08a28b12847c8e9c1
n-s-harvey <150607031+n-s-harvey@users.noreply.github.com>
2024-02-01 21:30:06 -0600

    Expand/collapse state refactored to Redux store

--- src/features/editor.js
+++ src/features/editor.js
@@ -1,20 +1,30 @@
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button'
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { updateInput } from './inputSlice';
+import { toggleExpanded } from './expandedSlice';
 
-export default function Editor(props) {
+export default function Editor() {
   const dispatch = useDispatch();
   const handleInputChange = (event) => {
     dispatch(updateInput(event.target.value))
   }
+  const handleExpandedChange = () => {
+    dispatch(toggleExpanded())
+  }
   const sampleMarkdown = useSelector(state => state.input.value);
+  const isExpanded = useSelector(state => state.expanded.value);
   return (
     <Container id='editor'>
       {/*TODO: update to maximize editor*/}
+      <Button variant="outline-primary" size="sm"
+        onClick={handleExpandedChange}>
+        {isExpanded ? 'Collapse' : 'Expand'}
+      </Button>
       <Form>
-        <Form.Control as="textarea" onChange={handleInputChange} defaultValue={sampleMarkdown} className={`${props.expanded ? 'expanded' : ''}`} />
+        <Form.Control as="textarea" onChange={handleInputChange} defaultValue={sampleMarkdown} className={`${isExpanded ? 'expanded' : ''}`} />
       </Form>
     </Container>
   );
