5ce6011fc2d11010f6099db5d68a594accac0380
n-s-harvey <150607031+n-s-harvey@users.noreply.github.com>
2024-02-04 07:16:52 -0600

    fCC testing bundle added

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
+      <script src='https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js' />
     </React.StrictMode>
   </Provider>
 );
